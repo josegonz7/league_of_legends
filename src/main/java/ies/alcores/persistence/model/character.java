4ecf676dea618ad739a9bb4123d7719f690c4781
josegonz7 <jgoncre1412@g.educaand.es>
2026-05-21 09:58:01 +0200

    creamos el modelo y vinculamos la base de datos

---
+++ src/main/java/ies/alcores/persistence/model/character.java
@@ -0,0 +1,24 @@
+package ies.alcores.persistence.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@Document(collection = "characters")
+public class character {
+    @Id
+    private String id;
+
+    private int championId;
+    private String name;
+    private String title;
+    @Field("role")
+    private List<String> roles;
+    private String thumbnailurl;
+}
