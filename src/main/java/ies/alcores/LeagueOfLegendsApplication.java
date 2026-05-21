77f4d677ffcecc64b9aae981c99c8fe6cb6364a0
josegonz7 <jgoncre1412@g.educaand.es>
2026-05-21 09:32:44 +0200

    comentario

--- src/main/java/ies/alcores/LeagueOfLegendsApplication.java
+++ src/main/java/ies/alcores/LeagueOfLegendsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LeagueOfLegendsApplication {
 
+	// com
 	public static void main(String[] args) {
 		SpringApplication.run(LeagueOfLegendsApplication.class, args);
 	}
